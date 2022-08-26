45af70cf4e783195a75bb8aec827ccc1dda439d8
jordan roth <jmtroth0@gmail.com>
2022-08-26 12:07:39 +0200

    setup config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,27 +1,32 @@
 import type { GatsbyConfig } from "gatsby";
 
+
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `portfolio`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: ["gatsby-plugin-sass", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": ""
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        title: `portfolio`,
+        siteUrl: `https://www.yourdomain.tld`
     },
-    __key: "images"
-  }]
+    graphqlTypegen: true,
+
+    plugins: [
+        "gatsby-plugin-sass",
+        {
+            resolve: 'gatsby-plugin-google-analytics',
+            options: {"trackingId": "UA-68572259-1"}
+        },
+        {
+            resolve: '@marceasen/gatsby-source-s3-image',
+            options: {
+                accessKeyId: "",
+                secretAccessKey: "",
+                bucketName: 'jmtr-portfolio-photos',
+                region: 'us-west-2',
+            },
+        },
+        "gatsby-plugin-image",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+    ]
 };
 
 export default config;
