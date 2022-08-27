99217365fb632cf21d289e0669e69db170cd7949
jordan roth <jmtroth0@gmail.com>
2022-08-27 14:41:40 +0200

    index is portfolio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,35 @@
 import * as React from "react"
+import { graphql } from "gatsby"
+import ImageList from "../components/image-list"
 
 
-const IndexPage = () => {
-  return (
-    <main>
-      <title>Hi</title>
-    </main>
-  )
+const PortfolioPage = (request) => {
+    return (
+        <main>
+        <title>Portfolio</title>
+        <ImageList photos={request.data.allS3ImageAsset.edges.map((edge) => {return edge.node.childImageSharp?.gatsbyImageData})}></ImageList>
+        </main>
+    )
 }
 
-export default IndexPage
+export default PortfolioPage;
+
+export const pageQuery = graphql`
+    query PhotographyPostsQuery {
+        allS3ImageAsset(filter: {childImageSharp: {id: {ne: null}}}) {
+            edges {
+                node {
+                    EXIF {
+                        DateCreatedISO
+                        ExposureTime
+                        FNumber
+                        ShutterSpeedValue
+                    }
+                    childImageSharp {
+                        gatsbyImageData
+                    }
+                }
+            }
+        }
+    }
+`
