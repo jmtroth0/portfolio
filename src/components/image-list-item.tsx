65c4340a7de216664f0df0fe2bec2d1caf5dd635
jordan roth <jmtroth0@gmail.com>
2022-08-28 18:42:48 +0000

    add link and hover effect

--- src/components/image-list-item.tsx
+++ src/components/image-list-item.tsx
@@ -1,10 +1,13 @@
 import * as React from "react"
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import { Link } from 'gatsby';
 
 const ImageListItem = (props) => {
     return (
         <li className="image-list-item">
-            <GatsbyImage className="image" image={getImage(props.photo)} alt="image" />
+            <Link href={ props.photo.images.fallback.src } target="_blank">
+                <GatsbyImage className="image" image={getImage(props.photo)} alt="image" />
+            </Link>
         </li>
     )
   }
